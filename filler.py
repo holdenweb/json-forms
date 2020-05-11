2e413e92fc4118e8407cd65f213f8cef6dc8d713
Steve Holden <steve@labster.com>
2020-05-11 19:35:02 +0100

    Re-work top-level logic to allow user cancellation.

--- filler.py
+++ filler.py
@@ -6,6 +6,10 @@ from forms import Form, Field, TextField, TimestampField
 
 from AnyQt.QtWidgets import QApplication, QMainWindow
 
+class MainWindow(QMainWindow):
+    def run(self):
+        pass
+
 form_data = [
     {'name': 'timestamp', 'type': TimestampField},
     {'name': 'first', 'type': TextField},
@@ -14,7 +18,8 @@ form_data = [
 ]
 app = QApplication(sys.argv)
 form = Form([x['type'](x['name']) for x in  form_data])
-main_window = QMainWindow()
-gui = ObjectFiller(form, parent=main_window)
-app.exec_()
-print(gui.get_value())
+gui = ObjectFiller(form, parent=None)
+if gui.exec_():
+    print(gui.get_value())
+else:
+    sys.exit("Cancelled by user")
